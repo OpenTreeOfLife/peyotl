5766a1a89ee9c611c7a098a0a7aa74c0e11cefdd
Mark T. Holder <mtholder@gmail.com>
2014-02-16 20:12:17 -0600

    untested list manipulations

--- peyotl/struct_diff.py
+++ peyotl/struct_diff.py
@@ -24,7 +24,7 @@ class DictDiff(object):
         r = []
         for k, v in self._modifications:
             pk = '{p}[{k}]'.format(p=par, k=repr(k))
-            if isinstance(v, DictDiff):
+            if isinstance(v, DictDiff) or isinstance(v, ListDiff):
                 sedits = v.edits_expr(par=pk)
                 r.extend(sedits)
             else:
@@ -49,7 +49,7 @@ class DictDiff(object):
         for k, v in self._additions:
             src[k] = v
         for k, v in self._modifications:
-            if isinstance(v, DictDiff):
+            if isinstance(v, DictDiff) or isinstance(v, ListDiff):
                 v.patch(src[k])
             else:
                 src[k] = v
@@ -102,7 +102,9 @@ class DictDiff(object):
 
 class ListDiff(object):
     def __init__(self):
-        pass
+        self._additions = []
+        self._modifications = []
+        self._deletions = []
     @staticmethod
     def create(src, dest):
         '''Inefficient comparison of src and dest dicts.
@@ -119,66 +121,145 @@ class ListDiff(object):
         Returned dicts may alias objects in src, and dest
         '''
         if src == dest:
-            return (True, None, None, None)
+            return None
         #TODO: find best match in list
         trivial_order = [(i, i) for i in range(min(len(src), len(dest)))]
         optimal_order = trivial_order
         src_ind = 0
         dest_ind = 0
         add_offset = 0
-        modl, addl, dell = [], [], []
+        num_deletions = 0
+        diffs = ListDiff()
         for p in optimal_order:
             ns, nd = p
             while src_ind < ns:
-                dell.append(ListDeletion(src_ind + add_offset, src[src_ind]))
+                diffs._add_deletion(src_ind, src[src_ind])
                 src_ind += 1
+                num_deletions += 1
             while dest_ind < nd:
-                addl.append(ListAddition(src_ind + add_offset, dest[dest_ind]))
+                diffs._add_insertion(src_ind - num_deletions, add_offset, dest[dest_ind])
                 dest_ind += 1
                 add_offset += 1
             sv, dv = src[ns], dest[nd]
             if sv != dv:
-                modl.append(ListElModification(src_ind + add_offset, sv, dv))
+                rec_call = None
+                if isinstance(sv, dict) and isinstance(dv, dict):
+                    rec_call = DictDiff.create(sv, dv, **kwargs)
+                elif isinstance(sv, list) and isinstance(dv, list):
+                    rec_call = ListDiff.create(sv, dv, **kwargs)
+                elif kwargs.get('wrap_dict_in_list', False):
+                    if isinstance(sv, dict) and isinstance(dv, list):
+                        rec_call = ListDiff.create([sv], dv, **kwargs)
+                    elif isinstance(dv, dict) or isinstance(sv, list):
+                        rec_call = ListDiff.create(sv, [dv], **kwargs)
+                if rec_call is None:
+                    diffs._add_modificaton(src_ind, rec_call)
+                else:
+                    diffs._add_modificaton(src_ind, (sv, dv))
             src_ind += 1
             dest_ind += 1
         while src_ind < len(src):
-            dell.append(ListDeletion(src_ind + add_offset, src[src_ind]))
+            diffs._add_deletion(src_ind, add_offset, src[src_ind])
             src_ind += 1
         while dest_ind < len(dest):
-            addl.append(ListAddition(src_ind + add_offset, dest[dest_ind]))
+            diffs._add_insertion(src_ind, add_offset, dest[dest_ind])
             dest_ind += 1
             add_offset += 1
-        return (False, modl, addl, dell)
+        diffs.finish()
+        return diffs
+    def edits_expr(self, par=''):
+        r = self.modification_expr(par=par)
+        r.extend(self.deletions_expr(par=par))
+        r.extend(self.additions_expr(par=par))
+        return r
+    def additions_expr(self, par=''):
+        r = []
+        for k, ld in self._additions:
+            post_del_ind = k[0] + k[1]
+            v = ld.obj
+            s = '{p}.insert({k:d}, {v})'.format(p=par, k=post_del_ind, v=repr(v))
+            r.append(s)
+        return r
+    def modification_expr(self, par=''):
+        r = []
+        for k, le in self._modifications:
+            v = le.obj
+            pk = '{p}[{k}]'.format(p=par, k=repr(k))
+            if isinstance(v, DictDiff) or isinstance(v, ListDiff):
+                sedits = v.edits_expr(par=pk)
+                r.extend(sedits)
+            else:
+                s = '{pk} = {v}'.format(pk=pk, v=repr(v[1]))
+                r.append(s)
+        return r
+    def deletions_expr(self, par=''):
+        # _deletions are reverse sorted
+        r = []
+        for k, ld in self._deletions:
+            s = '{p}.pop({k:d})'.format(p=par, k=k)
+            r.append(s)
+        return r
+    def _add_addition(self, k, v):
+        self._additions.append((k, v))
+    def _add_deletion(self, k, v):
+        self._deletions.append((k, v))
+    def _add_modification(self, k, v):
+        self._modifications.append((k, v))
+    def patch(self, src):
+        for k, v in self._modifications:
+            if isinstance(v, DictDiff) or isinstance(v, ListDiff):
+                v.patch(src[k])
+            else:
+                dest = v[1]
+                src[k] = dest
+        for k, v in self._deletions:
+            src.remove(k)
+        for k, ld in self._additions:
+            post_del_ind = k[0] + k[1]
+            v = ld.obj
+            src.insert(post_del_ind, v)
+    def _add_deletion(self, ind, obj):
+        tup = (ind, ListDeletion(ind, obj))
+        self._deletions.append(tup)
+    def _add_insertion(self, ind, add_offset, obj):
+        sortable_key = (ind, add_offset)
+        tup = (sortable_key, ListAddition(ind, add_offset, obj))
+        self._additions.append(tup)
+    def _add_modificaton(self, ind, obj):
+        tup = (ind, ListElModification(ind, obj))
+        self._modifications.append(tup)
+    def finish(self):
+        self._modifications.sort()
+        self._deletions.sort(reverse=True)
+        self._additions.sort()
+class ListEdit(object):
+    def __init__(self, src_ind, obj):
+        self.src_index = src_ind
+        self.obj = obj
 
-class ListDiffEdit(object):
-    pass
-
-class ListDeletion(ListDiffEdit):
-    def __init__(self, s_ind, o):
-        self.index = s_ind
-        self.obj = o
+class ListDeletion(ListEdit):
+    def __init__(self, src_ind, obj):
+        ListEdit.__init__(self, src_ind, obj)
     def __repr__(self):
-        return 'ListDeletion({s}, {o})'.format(s=self.index, o=repr(self.obj))
+        return 'ListDeletion({s}, {o})'.format(s=self.src_index, o=repr(self.obj))
     def __str__(self):
         return repr(self) 
-class ListAddition(ListDiffEdit):
-    def __init__(self, s_ind, o):
-        self.index = s_ind
-        self.obj = o
+
+class ListAddition(ListEdit):
+    def __init__(self, src_ind, add_offset, obj):
+        ListEdit.__init__(self, src_ind, obj)
+        self.add_offset = add_offset
     def __repr__(self):
-        return 'ListAddition({s}, {o})'.format(s=self.index, o=repr(self.obj))
+        return 'ListAddition({s}, {o})'.format(s=self.src_index, o=repr(self.obj))
     def __str__(self):
         return repr(self)
 
-class ListElModification(ListDiffEdit):
-    def __init__(self, s_ind, src, dest):
-        self.index = s_ind
-        self.src = src
-        self.dest = dest
+class ListElModification(ListEdit):
+    def __init__(self, src_ind, obj):
+        ListEdit.__init__(self, src_ind, obj)
     def __repr__(self):
-        return 'ListElModification({s}, {o}, {d})'.format(s=self.index,
-                                                          o=repr(self.src),
-                                                          d=repr(self.dest))
+        return 'ListElModification({s}, {o}, {d})'.format(s=self.src_index,
+                                                          o=repr(self.obj))
     def __str__(self):
         return repr(self)
 

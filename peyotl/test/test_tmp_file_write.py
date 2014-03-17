16522b77c79c9a03a3e675297b187574e320459b
Emily Jane McTavish <ejmctavish@gmail.com>
2014-03-17 16:51:29 -0500

    updated test

--- peyotl/test/test_tmp_file_write.py
+++ peyotl/test/test_tmp_file_write.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from peyotl.phylesystem.git_actions import GitAction
+from peyotl import phylesystem
 import unittest
 import codecs
 import json
@@ -8,11 +9,12 @@ from peyotl.test.support import pathmap
 
 n = read_as_json(pathmap.json_source_path('1003.json'))
 
-repodir="/Users/ejmctavish/Documents/projects/otapi/phylesystem_test"
+reponame = phylesystem.get_repos().keys()[0]
+repodir = phylesystem.get_repos()[reponame]
 
 class TestCreate(unittest.TestCase):
         gd=GitAction(repodir)
-        gd.write_study(study_id="9999", content=n, branch="ejm_test")
+        gd.write_study(study_id="1003", content=n, branch="git_actions_test_1003")
         
 if __name__ == "__main__":
     unittest.main()

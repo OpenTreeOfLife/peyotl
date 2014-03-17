c505058cc4d7eb72e37923b0e0b9b7dfbcc56921
Emily Jane McTavish <ejmctavish@gmail.com>
2014-03-17 15:52:15 -0500

    changed hardcoded paths to pathmaps

--- peyotl/test/test_tmp_file_write.py
+++ peyotl/test/test_tmp_file_write.py
@@ -3,20 +3,15 @@ from peyotl.phylesystem.git_actions import GitAction
 import unittest
 import codecs
 import json
+from peyotl.nexson_syntax import read_as_json
 
-inpf = codecs.open('/Users/ejmctavish/Documents/projects/otapi/api.opentreeoflife.org/ws-tests/data/1003.json', 'rU', encoding='utf-8')
-n = json.load(inpf)
-
+n = read_asjson(pathmap.nexson_source_path('1003'))
 
 repodir="/Users/ejmctavish/Documents/projects/otapi/phylesystem_test"
-#jsonfi="/Users/ejmctavish/Documents/projects/otapi/peyotl/peyotl/test/data/nexson/9/v1.2.json"
-#content=read_json(jsonfi)#.readlines()
-#study_id='9'
-#branch="ejm_test"
 
 class TestCreate(unittest.TestCase):
         gd=GitAction(repodir)
         gd.write_study(study_id="9999", content=n, branch="ejm_test")
         
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()git 

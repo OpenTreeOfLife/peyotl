8ce9829f3f9b745a37df6c8e6eca12190f3e57be
Mark T. Holder <mtholder@gmail.com>
2014-03-17 21:20:10 -0500

    Coercing phylografter str bools to bool.

--- dev/phylografter_meta_workaround.py
+++ dev/phylografter_meta_workaround.py
@@ -22,5 +22,39 @@ def rec_resource_meta(blob, k):
         for inner_k, v in blob.items():
             if isinstance(v, list) or isinstance(v, dict):
                 rec_resource_meta(v, inner_k)
+
+def coerce_boolean(blob, k):
+    '''Booleans emitted as "true" or "false"
+    for "@root" and "ot:isLeaf" meta
+    '''
+    if isinstance(blob, dict):
+        if k == 'meta':
+            if blob.get('@property') == 'ot:isLeaf':
+                v = blob.get('$')
+                try:
+                    if v.lower() == "true":
+                        blob['$'] = True
+                    elif v.lower == "false":
+                        blob['$'] = False
+                except:
+                    pass
+        else:
+            r = blob.get('@root')
+            if r is not None:
+                try:
+                    if r.lower() == "true":
+                        blob['@root'] = True
+                    elif r.lower == "false":
+                        blob['@root'] = False
+                except:
+                    pass
+        for inner_k, v in blob.items():
+            if isinstance(v, list) or isinstance(v, dict):
+                coerce_boolean(v, inner_k)
+    elif isinstance(blob, list):
+        for i in blob:
+            coerce_boolean(i, k)
 rec_resource_meta(obj, 'root')
-write_as_json(obj, out, indent=0)
+coerce_boolean(obj, 'root')
+
+write_as_json(obj, out)

ba05da8513191c3e71137057639cb372981ddd49
Mark T. Holder <mtholder@gmail.com>
2014-05-24 10:15:33 -0500

    refactoring and simple test of phylografter

---
+++ peyotl/test/test_phylografter.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+from peyotl.api import Phylografter
+from peyotl.nexson_syntax.helper import detect_nexson_version, find_val_literal_meta_first
+from peyotl.test.support.pathmap import get_test_ot_service_domains
+from peyotl.utility import get_logger
+import unittest
+import copy
+
+_LOG = get_logger(__name__)
+
+class TestDictDiff(unittest.TestCase):
+    def setUp(self):
+        d = get_test_ot_service_domains()
+        self.phylografter = Phylografter(d)
+
+    def testFetchStudy(self):
+        x = self.phylografter.fetch_study('252')
+        sid = find_val_literal_meta_first(x['nexml'], 'ot:studyId', detect_nexson_version(x))
+        self.assertTrue(sid in ['252', 'pg_252'])
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ peyotl/utility/io.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''Simple utility functions for Input/Output do not depend on any other part of
+peyotl.
+'''
+import codecs
+import os
+
+def open_for_group_write(fp, mode, encoding='utf-8'):
+    '''Open with mode=mode and permissions '-rw-rw-r--' group writable is 
+    the default on some systems/accounts, but it is important that it be present on our deployment machine
+    '''
+    d = os.path.split(fp)[0]
+    if not os.path.exists(d):
+        os.makedirs(d)
+    o = codecs.open(fp, mode, encoding=encoding)
+    o.flush()
+    os.chmod(fp, stat.S_IRGRP | stat.S_IROTH | stat.S_IRUSR | stat.S_IWGRP | stat.S_IWUSR)
+    return o
+
+def write_to_filepath(content, filepath, encoding='utf-8', mode='w', group_writeable=False):
+    '''Writes `content` to the `filepath` Creates parent directory
+    if needed, and uses the specified file `mode` and data `encoding`.
+    If `group_writeable` is True, the output file will have permissions to be
+        writable by the group (on POSIX systems)
+    '''
+    par_dir = os.path.split(filepath)[0]
+    if not os.path.exists(par_dir):
+        os.makedirs(par_dir)
+    if group_writeable:
+        with open_for_group_write(filepath, mode=mode, encoding=encoding) as fo:
+            fo.write(content)
+    else:
+        with codecs.open(filepath, mode=mode, encoding=encoding) as fo:
+            fo.write(content)
+
+def expand_path(p):
+    return os.path.expanduser(os.path.expandvars(p))
+
+def download(url, encoding='utf-8'):
+    import requests
+    response = requests.get(url)
+    response.encoding = encoding
+    return response.text

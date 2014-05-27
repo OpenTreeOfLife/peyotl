8cceb689f1a7915d6a093c21d4172ee614355e43
Mark T. Holder <mtholder@gmail.com>
2014-05-27 13:54:45 -0500

    tests of taxomachine wrapping

---
+++ peyotl/test/test_taxomachine.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+from peyotl.api import Taxomachine
+from peyotl.test.support.pathmap import get_test_ot_service_domains
+from peyotl.utility import get_logger
+import unittest
+
+_LOG = get_logger(__name__)
+
+class TestDictDiff(unittest.TestCase):
+    def setUp(self):
+        d = get_test_ot_service_domains()
+        self.taxomachine = Taxomachine(d)
+    def testContexts(self):
+        cdict = self.taxomachine.contexts()
+        print cdict
+        self.assertTrue('PLANTS' in cdict)
+    def xtestBogusName(self):
+        resp = self.taxomachine.TNRS('bogustaxonomicname')
+        self.assertEqual(resp, [])
+    def testSkunkName(self):
+        name = u'Mephitis mephitis'
+        resp = self.taxomachine.TNRS(name, 'Mammals')
+        self.assertEqual(len(resp), 1)
+        el = resp[0]
+        self.assertEqual(el['name'], name)
+        self.assertTrue(el['exact'])
+    def testHomonymName(self):
+        name = 'Nandina'
+        resp = self.taxomachine.TNRS(name)
+        self.assertEqual(len(resp), 2)
+        resp = self.taxomachine.TNRS(name, 'Animals')
+        self.assertEqual(len(resp), 1)
+        resp = self.taxomachine.TNRS(name, 'Flowering plants')
+        self.assertEqual(len(resp), 1)
+
+if __name__ == "__main__":
+    unittest.main()

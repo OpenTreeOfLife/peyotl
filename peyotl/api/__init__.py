2f5ed2aae185000747e8a4af44d66de97d3a816a
Mark T. Holder <mtholder@gmail.com>
2014-05-28 08:28:19 -0500

    refactored taxomachine wrapper to its own file

--- peyotl/api/__init__.py
+++ peyotl/api/__init__.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python
 from peyotl.api.wrapper import *
+from peyotl.api.taxomachine import Taxomachine
--- peyotl/api/wrapper.py
+++ peyotl/api/wrapper.py
@@ -76,6 +76,7 @@ class APIWrapper(object):
         return self._doc_store
     doc_store = property(get_doc_store)
     def get_taxomachine(self):
+        from peyotl.api.taxomachine import _TaxomachineAPIWrapper
         if self._taxomachine is None:
             self._taxomachine = _TaxomachineAPIWrapper(self.domains.taxomachine)
         return self._taxomachine
@@ -223,109 +224,6 @@ variable to obtain this token. If you need to obtain your key, see the instructi
         uri = '{d}/v1/study/{i}'.format(d=self.domain, i=study_id)
         return self._get(uri)
 
-class _TaxomachineAPIWrapper(_WSWrapper):
-    '''Wrapper around interactions with the taxomachine TNRS.
-    The primary service is TNRS (for taxonomic name resolution service)
-        which takes a name matches it to OTT
-    
-    In this wrapper implementation, he naming contexts are cached in:
-        _contexts as the raw return (dictionary of large group name
-            to context name within that group), and 
-        _valid_contexts a set of all context names.
-    For example in May of 2014, the contexts are:
-        {
-        'PLANTS': ['Land plants', 
-                   'Hornworts',
-                   'Mosses',
-                   'Liverworts',
-                   'Vascular plants',
-                   'Club mosses',
-                   'Ferns',
-                   'Seed plants',
-                   'Flowering plants',
-                   'Monocots',
-                   'Eudicots',
-                   'Asterids',
-                   'Rosids'],
-        'LIFE': ['All life'],
-        'ANIMALS': ['Animals',
-                    'Birds',
-                    'Tetrapods',
-                    'Mammals',
-                    'Amphibians',
-                    'Vertebrates',
-                    'Arthropods',
-                    'Molluscs',
-                    'Platyhelminthes',
-                    'Annelids',
-                    'Cnidarians',
-                    'Arachnides',
-                    'Insects'],
-        'BACTERIA': ['Bacteria'],
-        'FUNGI': ['Fungi']
-        }
-
-    
-    https://github.com/OpenTreeOfLife/opentree/blob/master/neo4j_services_docs.md
-
-    NOTES
-        need a get_OTT_version method in taxomachine.
-        contextQueryForNames args are confusing
-        includeDublious for autocompleteBoxQuery ?
-        use case for getContextForNames
-        Is there a use case for getNodeIDJSONFromName if we don't support CQL?
-        Is there any significance to the order of return for autocompleteBoxQuery ?
-    ott wrapper to add:
-        synonym finder ?
-        parent taxon ?
-        homonym finder ?
-    '''
-    def TNRS(self, name, contextName=None):
-        '''Takes a name and optional contextName returns a list of matches.
-        Each match is a dict with:
-           'higher' boolean DEF???
-           'exact' boolean for exact match
-           'ottId' int
-           'name'  name (or uniqname???) for the taxon in OTT
-           'nodeId' int ID of not in the taxomachine db. probably not of use to anyone...
-        '''
-        if contextName and contextName not in self.valid_contexts:
-            raise ValueError('"{}" is not a valid context name'.format(contextName))
-        uri = '{p}/autocompleteBoxQuery'.format(p=self.prefix)
-        data = {'queryString': name}
-        if contextName:
-            data['contextName'] = contextName
-        return self._post(uri, data=anyjson.dumps(data))
-    def __init__(self, domain):
-        self._contexts = None
-        self._valid_contexts = None
-        self.prefix = None
-        _WSWrapper.__init__(self, domain)
-        self.set_domain(domain)
-    def set_domain(self, d):
-        self._contexts = None
-        self._valid_contexts = None
-        self._domain = d
-        self.prefix = '{d}/taxomachine/ext/TNRS/graphdb'.format(d=d)
-    domain = property(_WSWrapper.get_domain, set_domain)
-    def contexts(self):
-        # Taxonomic name contexts. These are cached in _contexts
-        if self._contexts is None:
-            self._contexts = self._do_contexts_call()
-        return self._contexts
-    def _do_contexts_call(self):
-        uri = '{p}/getContextsJSON'.format(p=self.prefix)
-        return self._post(uri)
-    def _get_valid_contexts(self):
-        if self._valid_contexts is None:
-            c = self.contexts()
-            v = set()
-            for cn in c.values():
-                v.update(cn)
-            self._valid_contexts = v
-        return self._valid_contexts
-    valid_contexts = property(_get_valid_contexts)
-
 class _TreemachineAPIWrapper(_WSWrapper):
     def __init__(self, domain):
         _WSWrapper.__init__(self, domain)
@@ -422,9 +320,6 @@ def NexsonStore(domains=None):
 
 def Phylografter(domains=None):
     return APIWrapper(domains=domains).phylografter
-
-def Taxomachine(domains=None):
-    return APIWrapper(domains=domains).taxomachine
 
 def Treemachine(domains=None):
     return APIWrapper(domains=domains).treemachine

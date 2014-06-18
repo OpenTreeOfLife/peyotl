0665bf855851722408e6ee5044b449f30b6b3003
Mark T. Holder <mtholder@gmail.com>
2014-06-18 14:12:59 -0500

    much of the framework for building regexes - untested

--- peyotl/string/__init__.py
+++ peyotl/string/__init__.py
@@ -1,7 +1,278 @@
 #!/usr/bin/env python
+import re
 from peyotl import get_logger
 _LOG = get_logger(__name__)
 
+class FragType:
+    EMPTY = 0
+    HIGHER_TAXON_CASE_S = 1
+    TAXON_CASE_I = 2
+    EPITHET_CASE_S = 3
+    GENBANK_ACCESSION = 4
+    VAR = 5
+    SP = 6
+    SSP = 7
+    CF = 8
+    AFF = 9
+    SPACE = 10
+    WHITESPACE = 11
+    PUNCTUATION = 12
+    NUMBER = 12
+    UC_LETTER = 13
+    LC_LETTER = 14
+    START = 15
+    END = 16
+_HIGHER_TAXON_CS = '([-A-Z][a-z]{2,})'
+_TAXON_CI = '([-A-Za-z]{3,})'
+_EPITHET_CS = '([[-a-z]{3,})'
+_GENBANK = '([A-Z]{1,2}[0-9]{5,6})'
+_VAR = '([vV][aA][rR]\.?)'
+_SP = '([sS][pP]\.?)'
+_SSP = '([sS][sS][pP]\.?)'
+_CF = '([cC][fF]\.?)'
+_AFF = '([aA][fF][fF]\.?)'
+
+
+class OTULabelStringCruncher(object):
+    def __init__(self, pat_list):
+        self.pat_list = pat_list
+        self.gb_accession_index = None
+        for n, p in pat_list[1:-1:2]:
+            if p['code'] == FragType.GENBANK_ACCESSION:
+                self.gb_accession_index = (n - 1)/2 # each odd-indexed pattern will have one group
+        self.pattern = ''.join(pat_list)
+
+def _matches_all(word_list, pat):
+    for word in word_list:
+        if not pat.match(word):
+            return False
+    return True
+
+def attempt_to_create_taxonomic_regex_from_words(word_list, is_first):
+    '''Takes a series of words assumed to be taxonomic name fragments
+    returns a dict with:
+        `regex` : pattern with a group to capture the name
+        code: a facet of the FragType enum
+        '''
+    if not isinstance(word_list, set):
+        word_list = set(word_list)
+    if is_first:
+        if _matches_all(word_list, _HIGHER_TAXON_CS):
+            return {'regex': _HIGHER_TAXON_CS, 'code': FragType.HIGHER_TAXON_CASE_S, 'num_groups':1}
+    else:
+        if _matches_all(word_list, _VAR):
+            return {'regex': _VAR, 'code': FragType.VAR, 'num_groups':1}
+        if _matches_all(word_list, _SP):
+            return {'regex': _SP, 'code': FragType.SP, 'num_groups':1}
+        if _matches_all(word_list, _SSP):
+            return {'regex': _SSP, 'code': FragType.SSP, 'num_groups':1}
+        if _matches_all(word_list, _CF):
+            return {'regex': _CF, 'code': FragType.CF, 'num_groups':1}
+        if _matches_all(word_list, _AFF):
+            return {'regex': _AFF, 'code': FragType.AFF, 'num_groups':1}
+    if _matches_all(word_list, _GENBANK):
+        return {'regex': _GENBANK, 'code': FragType.CF, 'num_groups':1}
+    if _matches_all(word_list, _TAXON_CI):
+        return {'regex': _TAXON_CI, 'code': FragType.TAXON_CASE_I, 'num_groups':1}
+    return None
+
+_SPACE = re.compile(r'\s')
+_WHITESPACE = re.compile(r'\s')
+_PUNCTUATION_STR = r'[-~`<>._,;+?!@#$%^&()+={}|\\\[\]]'
+_PUNCTUATION = re.compile( _PUNCTUATION_STR)
+_NUMBER = re.compile(r'[0-9]')
+_UC_LETTER = re.compile(r'[A-Z]')
+_LC_LETTER = re.compile(r'[a-z]')
+
+def _char_set2char_class(cs):
+    if not isinstance(cs, set):
+        cs = set(cs)
+    if len(cs) == 1:
+        c = list(cs)[0]
+        if c == ' ':
+            return dict(pat=' ', code=FragType.SPACE)
+        if _WHITESPACE.match(c):
+            return dict(pat=r'\s', code=FragType.WHITESPACE)
+        if _PUNCTUATION.match(c):
+            return dict(pat=_PUNCTUATION_STR, code=FragType.PUNCTUATION)
+        if _NUMBER.match(c):
+            return dict(pat=r'[0-9]', code=FragType.NUMBER)
+        if _UC_LETTER.match(c):
+            return dict(pat=r'[A-Z]', code=FragType.UC_LETTER)
+        if _LC_LETTER.match(c):
+            return dict(pat=r'[A-Z]', code=FragType.LC_LETTER)
+        raise NotImplementedError('regex for "{}"'.format(c))
+
+    if _matches_all(cs, _SPACE):
+        return dict(pat=' ', code=FragType.SPACE)
+    if _matches_all(cs, _WHITESPACE):
+        return dict(pat=r'\s', code=FragType.WHITESPACE)
+    if _matches_all(cs, _PUNCTUATION_STR):
+        return dict(pat=_PUNCTUATION_STR, code=FragType.PUNCTUATION)
+    if _matches_all(cs, _NUMBER):
+        return dict(pat=r'[0-9]', code=FragType.NUMBER)
+    if _matches_all(cs, _UC_LETTER):
+        return dict(pat=r'[A-Z]', code=FragType.UC_LETTER)
+    if _matches_all(cs, _LC_LETTER):
+        return dict(pat=r'[A-Z]', code=FragType.LC_LETTER)
+    return None
+
+
+def _midwords2char_class(word_list, start_ind, end_ind):
+    if not isinstance(cs, set):
+        cs = set(cs)
+    if len(cs) == 1:
+        c = list(cs)[0]
+        if c == ' ':
+            return dict(pat=' ', code=FragType.SPACE)
+        if _WHITESPACE.match(c):
+            return dict(pat=r'\s', code=FragType.WHITESPACE)
+        if _PUNCTUATION.match(c):
+            return dict(pat=_PUNCTUATION_STR, code=FragType.PUNCTUATION)
+        if _NUMBER.match(c):
+            return dict(pat=r'[0-9]', code=FragType.NUMBER)
+        if _UC_LETTER.match(c):
+            return dict(pat=r'[A-Z]', code=FragType.UC_LETTER)
+        if _LC_LETTER.match(c):
+            return dict(pat=r'[A-Z]', code=FragType.LC_LETTER)
+        raise NotImplementedError('regex for "{}"'.format(c))
+
+    if _matches_all(cs, _SPACE):
+        return dict(pat=' ', code=FragType.SPACE)
+    if _matches_all(cs, _WHITESPACE):
+        return dict(pat=r'\s', code=FragType.WHITESPACE)
+    if _matches_all(cs, _PUNCTUATION_STR):
+        return dict(pat=_PUNCTUATION_STR, code=FragType.PUNCTUATION)
+    if _matches_all(cs, _NUMBER):
+        return dict(pat=r'[0-9]', code=FragType.NUMBER)
+    if _matches_all(cs, _UC_LETTER):
+        return dict(pat=r'[A-Z]', code=FragType.UC_LETTER)
+    if _matches_all(cs, _LC_LETTER):
+        return dict(pat=r'[A-Z]', code=FragType.LC_LETTER)
+    return None
+
+def attempt_to_create_intervening_regex_from_words(preceding, word_list, following):
+    if not isinstance(word_list, set):
+        word_list = set(word_list)
+    non_empty = []
+    leading = set()
+    trailing = set()
+    has_empty = False
+    for word in word_list:
+        if not word:
+            has_empty = True
+        else:
+            non_empty.append(word)
+            leading.add(word[0])
+            trailing.add(word[-1])
+
+    if not has_empty and _matches_all(non_empty, _GENBANK):
+        full_pat =  {'regex': _GENBANK, 'code': FragType.CF, 'num_groups':1}
+    else:
+        if preceding is None:
+            if not non_empty:
+                return {'regex': '^', 'num_groups':0, 'code': FragType.EMPTY}
+            ws_ind = 0
+            leading_pat = dict(pat='^', code=FragType.START)
+        else:
+            ws_ind = 1
+            leading_pat = _char_set2char_class(leading)
+            if leading_pat is None:
+                return None
+        if following is None:
+            if not non_empty:
+                return {'regex': '$', 'num_groups':0, 'code': FragType.EMPTY}
+            trail_pat = dict(pat='$', code=FragType.END)
+            we_ind = None
+        else:
+            we_ind = -1
+            trail_pat = _char_set2char_class(trailing)
+            if trail_pat is None:
+                return None
+        mid_pat = _midwords2char_class(non_empty, ws_ind, we_ind)
+        if mid_pat is None:
+            return None
+        full_pat = (leading_pat, mid_pat, trail_pat)
+    if not _can_transition(preceding, full_pat, following):
+        return None
+    if isinstance(full_pat, tuple):
+        return _join_patterns(full_pat)
+    return full_pat
+
+def attempt_to_create_taxonomic_regex_from_lib(save_odd_el_list):
+    '''Takes a list of lists of strings. The goal is to return a set of regex patterns
+    that will match all elements (where the element is the ''.joined string)
+    and have a group that will return the words in the odd numbered indices.
+    
+    assumes that all lists in save_odd_el_list are the same length
+    returns None or a list of OTULabelStringCruncher objects.
+    '''
+    if not save_odd_el_list:
+        return None
+    nw = len(save_odd_el_list[0])
+    assert (nw % 2) == 1
+    str_collections = [list() for i in range(nw)]
+    for el in save_odd_el_list:
+        assert len(el) == nw
+        for i, word in enumerate(el):
+            str_collections[i].append(word)
+    pat_list = [None for i in range(nw)]
+    for i in range(1, nw - 1, 2):
+        p = attempt_to_create_taxonomic_regex_from_words(str_collections[i])
+        if p is None:
+            return
+        pat_list[i] = p
+    for i in range(0, nw, 2):
+        if i == 0:
+            preceding = None
+        else:
+            preceding = pat_list[i - 1]
+        if i == (nw - 1):
+            following = None
+        else:
+            following = pat_list[i + 1]
+        p = attempt_to_create_intervening_regex_from_words(preceding, str_collections[i], following)
+        if p is None:
+            return
+        pat_list[i] = p
+    return OTULabelStringCruncher(pat_list)
+
+
+
+
+def create_library_of_intervening_fragments(input_output_list):
+    '''Takes a list of (input_str, output_str) tuples
+    Each output_str is expeceted to be a whitespace-delimited series of words (like the names in OTT)
+
+    returns a list. for each element in the list, there will be a series of lists
+    representing the output of find_intervening_fragments for output_str of that # or words
+    *with the expeceted words intercalated*
+
+    For example 'Homo sapiens' is 2 words, so the the input element:
+        ('blah Homo_sapiens+515', 'Homo sapiens')
+    would result in a n entry in the returned list at index 2. The content contributed
+    by this pair would be the list [['blah ', 'Homo', '_', 'sapiens', '+515']]
+    because find_intervening_fragments will return [['blah ', '_', '+515']]
+
+    This function assists in the creation of regex to match the expeceted patterns.
+    '''
+    library = [[]]
+    for input_str, output_str in input_output_list:
+        expected_str_list = output_str.split()
+        le = len(expected_str_list)
+        res = find_intervening_fragments(input_str, expected_str_list)
+        if res is not None:
+            while le >= len(library):
+                library.append([])
+            x = []
+            for el in res:
+                y = [el[0]]
+                for i in range(1, len(el)):
+                    y.append(expected_str_list[i - 1])
+                    y.append(el[i])
+                x.append(y)
+            library[le].append(x)
+    return library
 def find_intervening_fragments(input_str,
                                expected_str_list,
                                start_pos=0,
--- peyotl/test/test_string.py
+++ peyotl/test/test_string.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
-from peyotl.string import find_intervening_fragments
+from peyotl.string import find_intervening_fragments, \
+                          create_library_of_intervening_fragments
 from peyotl.test.support import pathmap
 from peyotl.utility import get_logger
 import unittest
@@ -22,5 +23,12 @@ class TestString(unittest.TestCase):
         self.assertEqual(len(f_list), 2)
         self.assertEqual(f_list[0], ['a', 'cbe', 'f'])
         self.assertEqual(f_list[1], ['abc', 'e', 'f'])
+    def testCreateLibraryInterveningFragments(self):
+        r = create_library_of_intervening_fragments([('blah Homo_sapiens+515', 'Homo sapiens'),
+                                                     ('blahhumbug Homo_sapiens+516', 'Homo sapiens')])
+        self.assertEqual(len(r), 3)
+        self.assertEqual(len(r[2]), 2)
+        self.assertEqual(r[2][0], [['blah ', 'Homo', '_', 'sapiens', '+515']])
+        self.assertEqual(r[2][1], [['blahhumbug ', 'Homo', '_', 'sapiens','+516']])
 if __name__ == "__main__":
     unittest.main(verbosity=5)

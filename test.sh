874f16f856dd16418b6dcb55ca4c1f69b42a5685
Mark T. Holder <mtholder@gmail.com>
2014-10-22 13:30:40 +0200

    Adds bracket_ingroup option to newick writing
    This used to be on by default, but the behavior
    is being changed to address:
        https://github.com/OpenTreeOfLife/opentree/issues/490
    
    test.sh moved to maintainer-test.sh so that the new
    test.sh can follow the new open tree convention of having a
    test.sh that installs and tests.

--- test.sh
+++ test.sh
@@ -1,31 +1,5 @@
 #!/bin/sh
-#set -x
-f=0 # the number of tests
-if ! ./integration-tests.sh
-then
-    f=1
-fi
-k=0
-if ! ./standalone-tests.sh
-then
-    k=1
-fi
-s=0
-if ! python setup.py test
-then
-    s=1
-fi
+pip install -r requirements.txt || exit 1
+python setup.py develop || exit 1
+python setup.py test || exit 1
 
-if test $f -eq 0
-then
-    echo "Passed all shell script tests."
-else
-    echo "Failed at least one shell script tests."
-fi
-if test $k -eq 0
-then
-    echo "Passed all standalone_tests passed."
-else
-    echo "Failed at least one standalone_test."
-fi
-exit $(expr $f + $k + $s)

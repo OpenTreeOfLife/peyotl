319f97a319cfb3fcb7454fc6d8e01c0c6d7784b8
Mark T. Holder <mtholder@gmail.com>
2014-11-14 03:30:55 +0100

    test to catch validation err in phylesystem-1 issue#2

---
+++ peyotl/test/test_nexson_validate_lacking_otus.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+from peyotl.nexson_syntax import detect_nexson_version, get_empty_nexson
+from peyotl.nexson_validation import validate_nexson
+from peyotl.test.support import pathmap
+from peyotl.utility import get_logger
+import unittest
+import codecs
+import json
+import os
+_LOG = get_logger(__name__)
+
+# round trip filename tuples
+VALID_NEXSON_DIRS = ['9', 'otu', ]
+
+def read_json(fp):
+    return json.load(codecs.open(fp, 'rU', encoding='utf-8'))
+def write_json(o, fp):
+    with codecs.open(fp, 'w', encoding='utf-8') as fo:
+        json.dump(o, fo, indent=2, sort_keys=True)
+        fo.write('\n')
+def through_json(d):
+    return json.loads(json.dumps(d))
+
+def dict_eq(a, b):
+    if a == b:
+        return True
+    return False
+    # ka, kb = a.keys(), b.keys()
+    # ka.sort()
+    # kb.sort()
+    # if ka != kb:
+    #     _LOG.debug('keys "{a}" != "{b}"'.format(a=ka, b=kb))
+    # for k in ka:
+    #     va = a[k]
+    #     vb = b[k]
+    #     if va != vb:
+    #         _LOG.debug('value for {k}: "{a}" != "{b}"'.format(k=k, a=va, b=vb))
+    # return False
+
+class TestConvert(unittest.TestCase):
+    def testInvalidFilesFail(self):
+        msg = ''
+        for fn in pathmap.all_files(os.path.join('nexson', 'lacking_otus')):
+            if fn.endswith('.input'):
+                frag = fn[:-len('.input')]
+                inp = read_json(fn)
+                aa = validate_nexson(inp)
+                annot = aa[0]
+                if len(annot.errors) == 0:
+                    ofn = pathmap.nexson_source_path(frag + '.output')
+                    ew_dict = annot.get_err_warn_summary_dict()
+                    write_json(ew_dict, ofn)
+                    msg = "Failed to reject file. See {o}".format(o=str(msg))
+                    self.assertTrue(False, msg)
+
+if __name__ == "__main__":
+    unittest.main()

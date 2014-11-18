6fbff37f4fcbbea215cc09fb8c6332324bb9a94c
Mark T. Holder <mtholder@gmail.com>
2014-11-18 09:47:45 +0100

    cleaning up imports to avoid circular dependencies in subpackages

--- dev/detect_intra_peyotl_dependencies.py
+++ dev/detect_intra_peyotl_dependencies.py
@@ -7,7 +7,10 @@ if '-h' in sys.argv:
     sys.stdout.write('''script to parse import statements in peyotl to determine
 which subpackages depend on which other parts.
 
-Args: -d writes the output in dot format.
+Args:
+    -d writes the output in dot format.
+    -s simplifies the output by only listing a higher level dependency (if that
+        dependency includes a lower level one)
 
 We want peyotl to be series of modules rather than an intertangled mass. Most
     packages depend on peyotl.utility and many will depend on peyotl.nexson_syntax
@@ -20,34 +23,55 @@ This script relies on the code using the "from peyotl... import" syntax (which w
 ''')
     sys.exit(0)
 write_dot = '-d' in sys.argv
+simplify = '-s' in sys.argv
 out = sys.stdout
 script_path = os.path.abspath(sys.argv[0])
 dev_path = os.path.split(script_path)[0]
 par_path = os.path.split(dev_path)[0]
 peyotl_path = os.path.join(par_path, 'peyotl')
-c = check_output(['grep', '-r', '^from peyotl.* import ', peyotl_path])
-mod_dep_mod = re.compile(r'^([a-zA-Z0-9._]+)[:/]\S*from peyotl\.([_a-zA-Z0-9]+)')
+c = check_output(['grep', '-r', '^ *from peyotl.* import ', peyotl_path])
+mod_dep_mod = re.compile(r'^ *([a-zA-Z0-9._]+)[:/]\S* *from peyotl\.([_a-zA-Z0-9]+)')
 d = {}
+sub_els = os.listdir(peyotl_path)
 for line in c.split('\n'):
     if '~' in line or '.pyc' in line:
         continue
     if line.startswith(peyotl_path):
         trimmed = line[1 + len(peyotl_path):-1]
         mt = mod_dep_mod.match(trimmed)
+        sys.stderr.write('trimmed = ' + trimmed + '\n')
         assert mt
         mod, dep = mt.group(1), mt.group(2)
-        gd = d.setdefault(mod, set())
-        gd.add(dep)
-
-for i in os.listdir(peyotl_path):
+        if mod != dep:
+            if mod.endswith('.py'):
+                mod = mod[:-3]
+            if dep.endswith('.py'):
+                dep = dep[:-3]
+            gd = d.setdefault(mod, set())
+            gd.add(dep)
+for i in sub_els:
     if '~' in i or i.endswith('.pyc'):
         continue
     if i not in d:
         d[i] = {}
-
-for suppress in ['test', '__init__.py']:
+def simplify_list_of_deps(dep_dict, dep_list):
+    remove = set()
+    for i in dep_list:
+        for j in dep_list:
+            if (i != j) and (i in dep_dict.get(j, {})):
+                #sys.stderr.write('{i} removed because it is in {j} deps\n'.format(i=i, j=j))
+                remove.add(i)
+    return set([i for i in dep_list if i not in remove])
+for suppress in ['test', '__init__']:
     if suppress in d:
         del d[suppress]
+if simplify:
+    r = {}
+    for k, v in d.items():
+        r[k] = simplify_list_of_deps(d, v)
+        #sys.stderr.write(k + ' -> ' + str(v) + '\n to    -> ' + str(r[k]) + '\n')
+    d = r
+
 ml = list(d.keys())
 ml.sort()
 

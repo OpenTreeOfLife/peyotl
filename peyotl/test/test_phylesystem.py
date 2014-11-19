8387c999a662cf06d850d74d66930108e2fc6b30
Mark T. Holder <mtholder@gmail.com>
2014-11-19 10:02:20 +0100

    making test do more checking, less printing

--- peyotl/test/test_phylesystem.py
+++ peyotl/test/test_phylesystem.py
@@ -5,15 +5,15 @@ from peyotl.test.support import pathmap
 import os
 _repos = pathmap.get_test_repos()
 ms, mp = _repos['mini_system'], _repos['mini_phyl']
-
+print ms, mp
 #pylint: disable=W0212
-@unittest.skipIf((not os.path.isdir(ms)) or (not os.path.isdir(ms)),
+@unittest.skipIf((not os.path.isdir(ms)) or (not os.path.isdir(mp)),
                  'Peyotl not configured for maintainer test of mini_phyl/system.' \
                  'Skipping this test is normal (for everyone other than MTH and EJBM).')
 class TestPhylesystem(unittest.TestCase):
     def setUp(self):
         self.r = dict(_repos)
-        print(self.r)
+        #print(self.r)
     def testInit(self):
         p = _Phylesystem(repos_dict=self.r)
         self.assertEqual(2, len(p._shards))
@@ -24,17 +24,18 @@ class TestPhylesystem(unittest.TestCase):
         self.assertEqual(k, ['10', '11', '12', '9'])
     def testURL(self):
         p = _Phylesystem(repos_dict=self.r)
-        print(p.get_public_url('9'))
+        self.assertTrue(p.get_public_url('9').endswith('9.json'))
     def testStudyIds(self):
         p = _Phylesystem(repos_dict=self.r)
-        print(p.get_study_ids())
+        k = list(p.get_study_ids())
+        k.sort()
+        self.assertEqual(k, ['10', '11', '12', '9'])
     def testNextStudyIds(self):
         p = _Phylesystem(repos_dict=self.r)
         nsi = p._mint_new_study_id()
         self.assertTrue(nsi.startswith('ot_'))
         r = _Phylesystem(repos_dict=self.r, new_study_prefix='ab_')
         nsi = r._mint_new_study_id()
-        print(nsi)
         self.assertTrue(nsi.startswith('ab_'))
 
 

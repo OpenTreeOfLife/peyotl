1472ff6e54e8699c4b52ad3f93faa0f75e45f77d
Mark T. Holder <mtholder@gmail.com>
2014-11-27 10:37:31 +0100

    moving NexsonProxy out of manip.py

--- peyotl/test/test_nexson_proxy_tree.py
+++ peyotl/test/test_nexson_proxy_tree.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 from peyotl.nexson_syntax import extract_tree_nexson
-from peyotl.manip import NexsonTreeProxy
+from peyotl.nexson_proxy import NexsonTreeProxy
 from peyotl.test.support import pathmap
 from peyotl.utility import get_logger
 import unittest

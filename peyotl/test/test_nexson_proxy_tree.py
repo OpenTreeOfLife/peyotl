fc2cec219e14cb22bb77b241d7e6b95f361f4fa6
Mark T. Holder <mtholder@gmail.com>
2014-11-27 10:41:49 +0100

    moving NexsonProxy out of manip.py

--- peyotl/test/test_nexson_proxy_tree.py
+++ peyotl/test/test_nexson_proxy_tree.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 from peyotl.nexson_syntax import extract_tree_nexson
-from peyotl.nexson_proxy import NexsonTreeProxy
+from peyotl.nexson_proxy import NexsonProxy
 from peyotl.test.support import pathmap
 from peyotl.utility import get_logger
 import unittest
@@ -8,12 +8,11 @@ _LOG = get_logger(__name__)
 class TestProxy(unittest.TestCase):
     def setUp(self):
         blob = pathmap.nexson_obj('9/v1.2.json')
-        tree_id, tree, otus = extract_tree_nexson(blob, tree_id='tree1')[0]
-        self._tree = tree
-        self.ntp = NexsonTreeProxy(tree=tree, tree_id=tree_id, otus=otus)
-    def testIter(self):
-        nbi = self._tree['nodeById']
-        ebi = self._tree['edgeBySourceId']
+        self.np = NexsonProxy(nexson=blob)
+    def testTreeIter(self):
+        ntp = self.np.get_tree('tree1')
+        nbi = ntp['nodeById']
+        ebi = ntp['edgeBySourceId']
         ks = set(nbi.keys())
         its = set()
         edge_less = None
@@ -27,7 +26,7 @@ class TestProxy(unittest.TestCase):
                 s = node.edge['@source']
                 ed = ebi[s]
                 self.assertIs(node.edge, ed[node.edge_id])
-        self.assertEqual(self._tree['^ot:rootNodeId'], edge_less)
+        self.assertEqual(ntp['^ot:rootNodeId'], edge_less)
         self.assertEqual(ks, its)
 if __name__ == "__main__":
     unittest.main()

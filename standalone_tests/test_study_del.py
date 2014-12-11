7b644e021537a4ff905b41f3f8e3c4788a1a1651
Mark T. Holder <mtholder@gmail.com>
2014-12-11 15:55:18 +0100

    test to reveal the reinstating the study bug mentioned in prev commit

--- standalone_tests/test_study_del.py
+++ standalone_tests/test_study_del.py
@@ -66,15 +66,30 @@ class TestPhylesystemDel(unittest.TestCase):
         self.assertEquals(acount_at_sha, curr['nexml'].get("^acount", 0))
         curr, naked_get_sha, wip_map = phylesystem.return_study(_SID, commit_sha=v1bsha, return_WIP_map=True)
         self.assertEquals(acount_at_v1bsha, curr['nexml']["^acount"])
+        ga = phylesystem.create_git_action(_SID) # assert no raise
         sidl = phylesystem.get_study_ids()
         self.assertNotIn(_SID, sidl)
         ac += 1
         curr['nexml']["^acount"] = ac
+        ga = phylesystem.create_git_action(_SID)
         v4b = commit_and_try_merge2master(ga, curr, _SID, _AUTH, v1bsha)
         v4bsha = v4b['sha']
         self.assertTrue(v4b['merge_needed'])
         curr, naked_get_sha, wip_map = phylesystem.return_study(_SID, commit_sha=v4bsha, return_WIP_map=True)
         self.assertEquals(ac, curr['nexml']["^acount"])
+        ac += 1
+        curr['nexml']["^acount"] = ac
+        ga = phylesystem.create_git_action(_SID)
+        v5b = phylesystem.commit_and_try_merge2master(curr, _SID, _AUTH, v3bsha)
+        self.assertFalse(v5b['merge_needed'])
+        sidl = phylesystem.get_study_ids()
+        self.assertIn(_SID, sidl)
+        curr, naked_get_sha, wip_map = phylesystem.return_study(_SID, commit_sha=v5b['sha'], return_WIP_map=True)
+        self.assertEquals(ac, curr['nexml']["^acount"])
+        curr, naked_get_sha, wip_map = phylesystem.return_study(_SID, return_WIP_map=True)
+        self.assertEquals(ac, curr['nexml']["^acount"])
+        self.assertRaises(KeyError, phylesystem.return_study, _SID, commit_sha=v2bsha, return_WIP_map=True)
+        self.assertRaises(KeyError, phylesystem.return_study, _SID, commit_sha=v3bsha, return_WIP_map=True)
         
 if __name__ == "__main__":
     unittest.main()

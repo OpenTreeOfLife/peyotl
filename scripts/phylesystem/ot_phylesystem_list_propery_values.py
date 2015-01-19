c8445b842f8d6bc08e1efcb2e9a30768bc94e209
Mark T. Holder <mtholder@gmail.com>
2015-01-19 16:40:39 +0100

    search trees or study properties

--- scripts/phylesystem/ot_phylesystem_list_propery_values.py
+++ scripts/phylesystem/ot_phylesystem_list_propery_values.py
@@ -20,17 +20,18 @@ description = __doc__
 prog = os.path.split(sys.argv[0])[-1]
 parser = argparse.ArgumentParser(prog=prog, description=description)
 parser.add_argument('--set', action='store_true', default=False, required=False, help="report the set of values")
+parser.add_argument('--tree', action='store_true', default=False, required=False, help="search tree properties rather than study properties")
 parser.add_argument('property')
 args = parser.parse_args(sys.argv[1:])
 study_prop = args.property
 phy = Phylesystem()
 out = codecs.getwriter('utf-8')(sys.stdout)
 summarize_as_set = args.set
+check_trees = args.tree
 v_dict = defaultdict(int)
 
-for study_id, n in phy.iter_study_objs():
-    nexml = get_nexml_el(n)
-    o = nexml.get(study_prop)
+
+def process_val(o):
     if o is not None:
         if isinstance(o, dict):
             h = o.get('@href')
@@ -44,6 +45,14 @@ for study_id, n in phy.iter_study_objs():
             v_dict[v] += 1
         else:
             out.write(u'{i}: {v}\n'.format(i=study_id, v=v))
+
+for study_id, n in phy.iter_study_objs():
+    nexml = get_nexml_el(n)
+    if check_trees:
+        for trees_group_id, tree_id, tree in iter_trees(n):
+            process_val(tree.get(study_prop))
+    else:
+        process_val(nexml.get(study_prop))
 if summarize_as_set:
     as_list = [(v, k) for k, v in v_dict.items()]
     as_list.sort(reverse=True)

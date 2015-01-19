52ad61c32729b6c7e33849e6c979978ea42591ee
Mark T. Holder <mtholder@gmail.com>
2015-01-19 16:35:06 +0100

    updated old (unused) script

--- scripts/phylesystem/ot_phylesystem_list_study_property.py
+++ scripts/phylesystem/ot_phylesystem_list_study_property.py
@@ -8,16 +8,29 @@ For example:
 
 prints out the study's URL for each study that has this property set.
 '''
-from peyotl import phylesystem_study_objs
+from peyotl.phylesystem.phylesystem_umbrella import Phylesystem
+from peyotl.nexson_syntax import get_nexml_el
+from peyotl.manip import iter_trees
+from collections import defaultdict
+import argparse
 import codecs
 import sys
+import os
+description = __doc__
+prog = os.path.split(sys.argv[0])[-1]
+parser = argparse.ArgumentParser(prog=prog, description=description)
+parser.add_argument('--set', action='store_true', default=False, required=False, help="report the set of values")
+parser.add_argument('property')
+args = parser.parse_args(sys.argv[1:])
+study_prop = args.property
+phy = Phylesystem()
 out = codecs.getwriter('utf-8')(sys.stdout)
-try:
-    study_prop = sys.argv[1]
-except:
-    sys.exit(__doc__)
-for study_id, n in phylesystem_study_objs():
-    o = n.get(study_prop)
+summarize_as_set = args.set
+v_dict = defaultdict(int)
+
+for study_id, n in phy.iter_study_objs():
+    nexml = get_nexml_el(n)
+    o = nexml.get(study_prop)
     if o is not None:
         if isinstance(o, dict):
             h = o.get('@href')
@@ -27,4 +40,12 @@ for study_id, n in phylesystem_study_objs():
                 v = h
         else:
             v = unicode(o)
-        out.write(u'{i}: {v}\n'.format(i=study_id, v=v))
+        if summarize_as_set:
+            v_dict[v] += 1
+        else:
+            out.write(u'{i}: {v}\n'.format(i=study_id, v=v))
+if summarize_as_set:
+    as_list = [(v, k) for k, v in v_dict.items()]
+    as_list.sort(reverse=True)
+    for v, k in as_list:
+        out.write(u'"{k}" (seen {v:d} times)\n'.format(k=k, v=v))

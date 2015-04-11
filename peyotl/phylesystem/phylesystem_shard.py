d9dab6119ac4b342361541b4cfb52c92731b7c5a
Jim Allman <jim@ibang.com>
2015-04-11 00:43:03 -0400

    more detailed diagnostics (misnamed property?)

--- peyotl/phylesystem/phylesystem_shard.py
+++ peyotl/phylesystem/phylesystem_shard.py
@@ -79,9 +79,14 @@ class PhylesystemShardProxy(GitShard):
 
 def diagnose_repo_nexml2json(shard):
     """Optimistic test for Nexson version in a shard (tests first study found)"""
-    with shard._index_lock:
-        fp = shard.study_index.values()[0][2]
     _LOG = get_logger('TEST-'.format(__name__))
+    with shard._index_lock:
+        import pprint
+        _LOG.debug('shard.study_index.values:\n{}'.format(pprint.pformat(shard.study_index.values())))
+        _LOG.debug('shard.study_index:\n{}'.format(pprint.pformat(shard.study_index)))
+        _LOG.debug('shard.doc_index:\n{}'.format(pprint.pformat(shard.doc_index)))
+        _LOG.debug('shard._doc_index:\n{}'.format(pprint.pformat(shard._doc_index)))
+        fp = shard.study_index.values()[0][2]
     _LOG.debug('diagnose_repo_nexml2json with fp={}'.format(fp))
     with codecs.open(fp, mode='r', encoding='utf-8') as fo:
         fj = json.load(fo)

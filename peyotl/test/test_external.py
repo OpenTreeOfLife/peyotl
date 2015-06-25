61d5436041913070613b8cd677a44e9a6408b209
Mark T. Holder <mtholder@gmail.com>
2015-06-25 16:24:36 +0200

    Partial test
    not hooked up to the unittest harness, but this is what
    I used to diagnose https://github.com/OpenTreeOfLife/opentree/issues/641

---
+++ peyotl/test/test_external.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+from peyotl.external import get_ot_study_info_from_treebase_nexml
+from peyotl.test.support import pathmap
+from peyotl import write_as_json
+from peyotl.utility import get_logger
+import unittest
+import codecs
+_LOG = get_logger(__name__)
+
+class TestExternal(unittest.TestCase):
+    pass
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        with codecs.open(sys.argv[1], 'r', encoding='utf-8') as inp:
+            content = inp.read()
+        b = get_ot_study_info_from_treebase_nexml(nexml_content=content)
+        write_as_json(b, sys.stdout)
+    #unittest.main()

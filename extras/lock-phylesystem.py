a0fa3915c6f37eb6018277ae5b6bfcb6fdbbeb7a
Mark T. Holder <mtholder@gmail.com>
2015-10-04 12:05:31 -0500

    documentation

--- extras/lock-phylesystem.py
+++ extras/lock-phylesystem.py
@@ -3,13 +3,31 @@ import locket
 import sys
 import os
 
-repo = sys.argv[1]
-git_dir = os.path.join(repo, '.git')
-assert os.path.isdir(git_dir)
+try:
+    repo = sys.argv[1]
+    git_dir = os.path.join(repo, '.git')
+    assert os.path.isdir(git_dir)
+except:
+    sys.exit('''Expecting a phylesystem or collections shard as the only argument.
+This script looks for the .git dir in the first argument, and locks that .git
+  dir to prevent simultaneous operations by the phylesystem-api.
+
+An example usage would be:
+
+    $ ssh api
+    $ source venv/bin/activate
+    $ cd repo/phylesystem-1_par/phylesystem-1
+    $ python ~/repo/peyotl/extras/lock-phylesystem.py .
+    ...
+    <Ctrl-D> to release the lock.
+
+''')
 lf = os.path.join(git_dir, "API_WRITE_LOCK")
 with locket.lock_file(lf, timeout=10):
     print 'Lock acquired. Use Control-D to release'
-    x = sys.stdin.read()
-    print 'released'
+    try:
+      x = sys.stdin.read()
+    finally:
+      print 'Lock released'
     sys.exit(0)
 sys.exit('timeout waiting for lock\n')

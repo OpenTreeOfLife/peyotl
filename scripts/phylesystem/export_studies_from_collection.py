eb85730a071dc9fde22ae5b771344d3a0274c261
Mark T. Holder <mtholder@gmail.com>
2015-10-06 15:29:09 -0500

    snapshot script is a work in progress

--- scripts/phylesystem/export_studies_from_collection.py
+++ scripts/phylesystem/export_studies_from_collection.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
+from peyotl.collections import get_empty_collection
+from peyotl import collection_to_included_trees, read_as_json
+from peyotl.phylesystem import Phylesystem
+from collections import defaultdict
+import codecs
+import copy
+import sys
+import os
+
+def copy_phylesystem_file_if_differing(git_action, coll_decision, out_dir, cd_to_new_map):
+    study_id = coll_decision['studyID']
+    fp = git_action.path_for_doc(study_id)
+    print fp
+
 if __name__ == '__main__':
-    from peyotl import collection_to_included_trees, read_as_json
-    from peyotl.phylesystem import Phylesystem
     import argparse
-    import codecs
-    import sys
-    import os
     description = 'Takes an collection JSON and prints out information from it'
     parser = argparse.ArgumentParser(prog='suppress-dubious', description=description)
     parser.add_argument('--phylesystem-par',
@@ -17,7 +26,15 @@ if __name__ == '__main__':
                         default=None,
                         type=str,
                         help='filepath for the collections JSON')
+    parser.add_argument('--output-dir',
+                        default=None,
+                        type=str,
+                        required='False',
+                        help='filepath for the output directory')
     args = parser.parse_args(sys.argv[1:])
+    out_dir = '.' if args.output_dir is None else args.output_dir
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
     # Create a Phylesystem wrapper
     if args.phylesystem_par is not None:
         if not os.path.isdir(args.phylesystem_par):
@@ -37,4 +54,45 @@ if __name__ == '__main__':
     except:
         sys.stderr.write('Error: JSON parse error when reading collection "{}".\n'.format(args.collection))
         raise
-    print ps.get_configuration_dict()
+    included_by_sha = defaultdict(list)
+    for inc in included:
+        included_by_sha[inc['SHA']].append(inc)
+    # map id of input included tree to concrete form
+    generic2concrete = {}
+    use_latest_trees = included_by_sha['']
+    if use_latest_trees:
+        sha_to_inc = defaultdict(list)
+        for ult in use_latest_trees:
+            study_id = ult['studyID']
+            ga = ps.create_git_action(study_id)
+            sha = ga.get_master_sha()
+            sha_to_inc[sha].append(ult)
+        for sha, from_this_sha_inc in sha_to_inc.items():
+            inc = from_this_sha_inc.pop(0)
+            study_id = inc['studyID']
+            ga = ps.create_git_action(study_id)
+            ga.checkout_master()
+            copy_phylesystem_file_if_differing(ga, inc, out_dir, generic2concrete)
+            for inc in from_this_sha_inc:
+                ga = ps.create_git_action(study_id)
+                ga.checkout_master()
+                copy_phylesystem_file_if_differing(ga, inc, out_dir, generic2concrete)
+    for sha, from_this_sha_inc in included_by_sha.items():
+        if sha == '':
+            continue
+        for inc in from_this_sha_inc:
+            study_id = inc['studyID']
+            ga = ps.create_git_action(study_id)
+            ga.checkout(sha)
+            copy_phylesystem_file_if_differing(ga, inc, out_dir, generic2concrete)
+            ga.checkout_master()
+    sys.exit()
+    coll_name = os.path.split(args.collection)[-1]
+    concrete_collection = get_empty_collection()
+    concrete_collection['description'] = 'Concrete form of collection "{}"'.format(coll_name)
+    cd_list = concrete_collection['decisions']
+    for inc in included:
+        concrete = generic2concrete[id(inc)]
+        cd_list.append(concrete)
+    concrete_fn = os.path.join(out_dir, 'concrete-' + coll_name)
+    write_as_json(concrete_collection, concrete_fn)

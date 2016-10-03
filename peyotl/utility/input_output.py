22869457a61714a725cbab0221c6c19d8ccf017e
Mark T. Holder <mtholder@gmail.com>
2016-10-03 15:29:09 -0500

    Removing unused simple file lock and cleaning
    This finishes the cleaning of peyotl.utility.

--- peyotl/utility/input_output.py
+++ peyotl/utility/input_output.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
-'''Simple utility functions for Input/Output do not depend on any other part of
+"""Simple utility functions for Input/Output do not depend on any other part of
 peyotl.
-'''
+"""
 from peyotl.utility.str_util import is_str_type, StringIO
 import codecs
 import json
 import stat
 import os
 
+
 def open_for_group_write(fp, mode, encoding='utf-8'):
-    '''Open with mode=mode and permissions '-rw-rw-r--' group writable is
+    """Open with mode=mode and permissions '-rw-rw-r--' group writable is
     the default on some systems/accounts, but it is important that it be present on our deployment machine
-    '''
+    """
     d = os.path.split(fp)[0]
     if not os.path.exists(d):
         os.makedirs(d)
@@ -19,17 +20,20 @@ def open_for_group_write(fp, mode, encoding='utf-8'):
     o.flush()
     os.chmod(fp, stat.S_IRGRP | stat.S_IROTH | stat.S_IRUSR | stat.S_IWGRP | stat.S_IWUSR)
     return o
+
+
 def read_filepath(filepath, encoding='utf-8'):
+    """Returns the text content of `filepath`"""
     with codecs.open(filepath, 'r', encoding=encoding) as fo:
         return fo.read()
 
 
 def write_to_filepath(content, filepath, encoding='utf-8', mode='w', group_writeable=False):
-    '''Writes `content` to the `filepath` Creates parent directory
+    """Writes `content` to the `filepath` Creates parent directory
     if needed, and uses the specified file `mode` and data `encoding`.
     If `group_writeable` is True, the output file will have permissions to be
         writable by the group (on POSIX systems)
-    '''
+    """
     par_dir = os.path.split(filepath)[0]
     if not os.path.exists(par_dir):
         os.makedirs(par_dir)
@@ -40,16 +44,24 @@ def write_to_filepath(content, filepath, encoding='utf-8', mode='w', group_write
         with codecs.open(filepath, mode=mode, encoding=encoding) as fo:
             fo.write(content)
 
+
 def expand_path(p):
+    """Helper function to expand ~ and any environmental vars in a path string."""
     return os.path.expanduser(os.path.expandvars(p))
 
+
 def download(url, encoding='utf-8'):
+    """Returns the text fetched via http GET from URL, read as `encoding`"""
     import requests
     response = requests.get(url)
     response.encoding = encoding
     return response.text
 
+
 def write_as_json(blob, dest, indent=0, sort_keys=True):
+    """Writes `blob` as JSON to the filepath `dest` or the filestream `dest` (if it isn't a string)
+    uses utf-8 encoding if the filepath is given (does not change the encoding if dest is already open).
+    """
     opened_out = False
     if is_str_type(dest):
         out = codecs.open(dest, mode='w', encoding='utf-8')
@@ -64,13 +76,16 @@ def write_as_json(blob, dest, indent=0, sort_keys=True):
         if opened_out:
             out.close()
 
+
 def pretty_dict_str(d, indent=2):
-    '''shows JSON indented representation of d'''
+    """shows JSON indented representation of d"""
     b = StringIO()
     write_pretty_dict_str(b, d, indent=indent)
     return b.getvalue()
+
+
 def write_pretty_dict_str(out, obj, indent=2):
-    '''writes JSON indented representation of obj to out'''
+    """writes JSON indented representation of `obj` to `out`"""
     json.dump(obj,
               out,
               indent=indent,
@@ -79,17 +94,19 @@ def write_pretty_dict_str(out, obj, indent=2):
               ensure_ascii=False,
               encoding="utf-8")
 
-def read_as_json(infi, encoding='utf-8'):
-    with codecs.open(infi, 'r', encoding=encoding) as inpf:
-        n = json.load(inpf)
-    return n
+
+def read_as_json(in_filename, encoding='utf-8'):
+    with codecs.open(in_filename, 'r', encoding=encoding) as inpf:
+        return json.load(inpf)
+
 
 def parse_study_tree_list(fp):
-    '''study trees should be in {'study_id', 'tree_id'} objects, but
+    """study trees should be in {'study_id', 'tree_id'} objects, but
     as legacy support we also need to support files that have the format:
     pg_315_4246243 # comment
 
-    '''
+    """
+    # noinspection PyBroadException
     try:
         sl = read_as_json(fp)
     except:
@@ -106,12 +123,12 @@ def parse_study_tree_list(fp):
             assert 'tree_id' in element
             ret.append(element)
         else:
+            # noinspection PyUnresolvedReferences,PyUnresolvedReferences
             assert element.startswith('pg_') or element.startswith('ot_')
+            # noinspection PyUnresolvedReferences
             s = element.split('_')
             assert len(s) > 1
             tree_id = s[-1]
             study_id = '_'.join(s[:-1])
             ret.append({'study_id': study_id, 'tree_id': tree_id})
     return ret
-
-

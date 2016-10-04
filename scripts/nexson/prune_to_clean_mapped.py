0d8f324b31085bc6993ea08f2e8f3a0fe5de6eda
Mark T. Holder <mtholder@gmail.com>
2016-10-04 07:00:52 -0500

    Bug fixes in pruning forwarded IDs.
    Wrong var name for ott_id and failure to set value for key.
    The latter would have crashed if it had ever been triggered, because
    the dict is a property (so can't be set).
    The former would have crashed because it tried to double delete
    the ott_id key set in a previous loop (and already deleted there).

--- scripts/nexson/prune_to_clean_mapped.py
+++ scripts/nexson/prune_to_clean_mapped.py
@@ -299,13 +299,13 @@ class NexsonTreeWrapper(object):
             del self.by_ott_id[ott_id]
         for old_id, new_id in old2new.items():
             old_node_list = self.by_ott_id[old_id]
-            del self.by_ott_id[ott_id]
+            del self.by_ott_id[old_id]
             if new_id in self.by_ott_id:
                 v = self.by_ott_id[new_id]
                 v.extend(old_node_list)
                 v.sort()
             else:
-                self.by_ott_id = old_node_list
+                self.by_ott_id[new_id] = old_node_list
             for sortable_el in old_node_list:
                 otu = sortable_el[3]
                 assert otu['^ot:ottId'] == old_id

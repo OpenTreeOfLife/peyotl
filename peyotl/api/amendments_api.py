50e7d8b338c1422d61bf77341dae2a2ec2aae9c6
Jim Allman <jim@ibang.com>
2016-10-11 15:38:44 -0400

    Revert "Handle optional JSON wrapper in doi2url"
    This reverts commit 62790cdd8ac566263d3660621a147a90d0677faa.

--- peyotl/api/amendments_api.py
+++ peyotl/api/amendments_api.py
@@ -166,12 +166,7 @@ variable to obtain this token. If you need to obtain your key, see the instructi
                                     json):
         # Convert source DOIs to their URL form (when fetching, saving, or
         # updating an amendment)
-        if 'taxa' in json:
-            # simple amendment JSON
-            taxa = json.get('taxa')
-        else:
-            # JSON includes wrapper w/ history, etc.
-            taxa = json.get('data').get('taxa')
+        taxa = json.get('taxa')
         if isinstance(taxa, list):
             for taxon in taxa:
                 sources = taxon.get('sources')
@@ -179,7 +174,8 @@ variable to obtain this token. If you need to obtain your key, see the instructi
                     for src in sources:
                         if src.get('source_type') == "Link (DOI) to publication":
                             doi = src.get('source', "")
-                            src['source'] = doi2url(doi)
+                            src.set('source', doi2url(doi) )
+
     def post_amendment(self,
                        json,
                        commit_msg=None):

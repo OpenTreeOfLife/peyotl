41a15307511c1faaccd2a064b18c9a1f1bf5fed4
Mark T. Holder <mtholder@gmail.com>
2022-09-22 17:50:09 -0500

    commenting out logging

--- peyotl/ott/__init__.py
+++ peyotl/ott/__init__.py
@@ -880,7 +880,7 @@ class OTT(object):
         known_above_root, known_below_root = set(), set()
         oi2poi = self.ott_id2par_ott_id
         ft = self.forward_table
-        _LOG.debug("len(ft) = {}".format(len(ft)))
+        # _LOG.debug("len(ft) = {}".format(len(ft)))
         for old_id in ott_id_list:
             if old_id in oi2poi:
                 if self.check_if_above_root(old_id, known_above_root, known_below_root, root_ott_id):
@@ -893,7 +893,7 @@ class OTT(object):
             else:
                 new_id = ft.get(old_id)
                 if new_id is None:
-                    _LOG.debug("{o} not found in ott_id2par_ott_id or forward_table".format(o=type(old_id)))
+                    # _LOG.debug("{o} not found in ott_id2par_ott_id or forward_table".format(o=type(old_id)))
                     unrecog.append(old_id)
                 else:
                     new_was_found = False
